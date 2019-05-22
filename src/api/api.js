84fe619aa98a669826ec880f8e8c59deeeec9afb
澜立齐 <1532900523@qq.com>
2019-05-22 10:50:59 +0800

    axios 成功发送请求  并得到数据   promise解析成功    问题  awit没有等待promise解析直接掠过

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,43 @@
-// 封装axios  生成webapi类
+// 封装axios  生成webapi
+import axios from 'axios';
+import { Promise, reject } from 'q';
+import { Resolver } from 'dns';
+
+
+class WebApi {
+    constructor() {
+        this.request = axios
+    }
+    GET(url, data) {
+        var result = this.request({
+            url: url,
+            method: 'get',
+            parmas: data,
+        })
+        console.log(result)
+        this.AWP(result)
+    }
+    AWP(data) {
+        try {
+            var promise = new Promise((Resolver, reject) => {
+                Resolver(data)
+                reject(data)
+            })
+            var obj = promise.then((data) => {
+                if (data.status === 200) {
+                    console.log('操作成功   ' + data)
+                    return data
+                } else {
+                    console.log('数据库连接异常！   ' + data.data)
+                }
+            }).catch((data) => {
+                console.log('数据解析失败！！' + data)
+            })
+        } catch (error) {
+            console.log('解析出错')
+        }
+    }
+
+}
+
+export default WebApi
--- src/page/welcome.js
+++ src/page/welcome.js
@@ -1,12 +1,27 @@
 import React from 'react'
-
+import WebApi from './../api/api'
+import { async } from 'q';
 
 export default class WelcomePage extends React.Component {
     render() {
         return (
             <div>
+                <button onClick={getData}></button>
                 这个是欢迎页
             </div>
+
         )
     }
+    //      getData = (e) => {
+    //         var url = `http://192.168.1.234:9023/CompDeptHandler/AppHandler.ashx?OP=AppUserAccount`
+    //         var datap = new WebApi()
+    //         var result =  datap.GET(url, {})
+    // debugger
+    //     }
+}
+async function getData() {
+    var url = `http://localhost:3000/api/test/findAll`
+    var datap = new WebApi()
+    var result = await datap.GET(url, {})
+    console.log(result)
 }
