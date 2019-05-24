3e6c59e06d0c13eea8f40fda6de0c5e84cef87e9
澜立齐 <1532900523@qq.com>
2019-05-24 15:34:43 +0800

    引入   react-boorstrop 暂未使用

---
+++ src/page/logoin/login.js
@@ -0,0 +1,17 @@
+// 登录界面
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+import { } from 'react-bootstrap'
+
+class LogIn extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
